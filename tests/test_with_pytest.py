fd5abd4b71fd43947c778f8f56b8f09ea29f8ffb
ahmetserguns <ahmetserguns@gmail.com>
2021-02-02 18:43:00 +0300

    Update test_with_pytest.py

--- tests/test_with_pytest.py
+++ tests/test_with_pytest.py
@@ -6,19 +6,19 @@ def test_payment():
     assert a.showpayment()== 12909.964456062355
 
 def test_summary():
-    assert a.showsummary()==None
+    assert a.showsummary() is None
 
 def test_schedule():
-    assert a.showschedule() == None
+    assert a.showschedule() is None
 
 def test_int():
-    assert is_int("3.4")==False
+    assert is_int("3.4") is False
 
 def test_float():
-    assert is_float("4")==False
+    assert is_float("4") is False
 
 def test_number():
-    assert  is_number("1.3")==True
+    assert  is_number("1.3") is True
 
 def test_cashflow():
     assert a.cashflow()== [-150000, 12909.964456062355, 12909.964456062355, 12909.964456062355, 12909.964456062355,
