47dec547013a3bca047b4201d82280b50f140500
ahmetserguns <ahmetserguns@gmail.com>
2021-02-08 01:14:56 +0300

    Update test_with_pytest.py

--- tests/test_with_pytest.py
+++ tests/test_with_pytest.py
@@ -1,15 +1,5 @@
 
-from amortize.calc import Mortgage,is_int,is_float,is_number
-a=Mortgage(150000,6,12,0)
-
-def test_payment():
-    assert a.showpayment()== 12909.964456062355
-
-def test_summary():
-    assert a.showsummary() is None
-
-def test_schedule():
-    assert a.showschedule() is None
+from amortize.calc import is_int,is_float,is_number
 
 def test_int():
     assert is_int("3.4") is False
@@ -20,10 +10,6 @@ def test_float():
 def test_number():
     assert  is_number("1.3") is True
 
-def test_cashflow():
-    assert a.cashflow()== [-150000, 12909.964456062355, 12909.964456062355, 12909.964456062355, 12909.964456062355,
-                           12909.964456062355, 12909.964456062355, 12909.964456062355, 12909.964456062355,
-                           12909.964456062355, 12909.964456062355, 12909.964456062355, 12909.964456062355]
 
 
 
